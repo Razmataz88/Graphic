18d5130a4f18a4e64b205f514f5a31ff2aa7d0ab
Jim Diamond <Jim.Diamond@acadiau.ca>
2019-10-11 20:12:22 -0300

    Added header comment. Removed a redundant field "point_star" from the graph struct. Minor reformatting.

--- graph.h
+++ graph.h
@@ -1,3 +1,18 @@
+/*
+ * File:	graph.h
+ * Author:	Rachel Bood
+ * Date:	2014 or 2015?
+ * Version:	1.0
+ *
+ * Purpose:	Define the graph class.
+ *
+ * Modification history:
+ * Oct 10, 2019 (JD)
+ *  (a) Add this header comment.
+ *  (b) Remove unused field "point_star" from the graph struct.
+ *  (c) Minor reformatting.
+ */
+
 #ifndef GRAPH_H
 #define GRAPH_H
 
@@ -16,15 +31,13 @@ public:
         QList<Node *> cycle;
         QList<QList <Node *>> double_cycle;
         QList<QList <Node *>> list_of_cycles;
-        QList<Node *> point_star;
         QList<Node *> bipartite_top;
         QList<Node *> bipartite_bottom;
         QList<Node *> grid;
         QList<Node *> path;
         QVector<Node *> binaryHeap;
         Node * center;
-
-    }Nodes;
+    } Nodes;
 
     Graph();
     void isMoved();
@@ -34,14 +47,16 @@ public:
 
     QRectF boundingRect() const;
     void setRotation(qreal aRotation);
-    QGraphicsItem *getRootParent();
+    QGraphicsItem * getRootParent();
 
 protected:
-    void mouseReleaseEvent(QGraphicsSceneMouseEvent *event);
-    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+    void mouseReleaseEvent(QGraphicsSceneMouseEvent * event);
+    void paint(QPainter * painter,
+	       const QStyleOptionGraphicsItem * option,
+	       QWidget * widget);
 
 private:
-     int moved;
+     int moved;		// 1 means the graph was dropped onto the canvas.
 };
 
 #endif // GRAPH_H
