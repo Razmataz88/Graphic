e0714630883680445a18a40fcd6398086716de59
Jim Diamond <Jim.Diamond@acadiau.ca>
2019-11-13 15:39:14 -0400

    Fix incorrect #ifndef string.

--- labelcontroller.h
+++ labelcontroller.h
@@ -2,25 +2,27 @@
  * File:    labelcontroller.h
  * Author:  Rachel Bood
  * Date:    2014/11/07 (?)
- * Version: 1.1
+ * Version: 1.2
  *
  * Purpose: 
  *
  * Modification history:
- * Nov 13, 2019 (JD, V1.1)
- *  - rename "Weight" to "Label" for edge function names.
+ *  Nov 13, 2019 (JD, V1.1)
+ *   - rename "Weight" to "Label" for edge function names.
+ *  Nov 13, 2019 (JD, V1.2)
+ *   - fix incorrect #ifndef string.
  */
 
 
-#ifndef EDGEWEIGHTCONTROLLER_H
-#define EDGEWEIGHTCONTROLLER_H
+#ifndef LABELCONTROLLER_H
+#define LABELCONTROLLER_H
 #include "edge.h"
 #include "node.h"
 
 #include <QLineEdit>
 #include <QObject>
 
-class LabelController : public QObject
+class LabelController: public QObject
 {
     Q_OBJECT
 public:
@@ -38,4 +40,4 @@ private:
     QLineEdit * edit;
 };
 
-#endif // EDGEWEIGHTCONTROLLER_H
+#endif // LABELCONTROLLER_H
