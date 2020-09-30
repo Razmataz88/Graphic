ffc6f0238543130d5ea972b59854241ea128137f
Jim Diamond <Jim.Diamond@acadiau.ca>
2020-09-30 20:12:20 -0300

    INSTALL:
    main.cpp:
     * May 12, 2020 (IC V1.2)
     *  (a) Added another font (arimo.ttf) to be embedded, so that Graphic
     *      can look as similar as possible on different systems.
    
    mainwindow.ui:
     * May 15, 2020 (IC)
        Tweaking of UI to look better across different systems.
    
    INSTALL:
     * May 15, 2020 (IC)
        Added instructions for windoze.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
  * File:    main.cpp
  * Author:  Rachel Bood 100088769
  * Date:    2014/11/07
- * Version: 1.1
+ * Version: 1.2
  *
  * Purpose: executes the mainwindow.ui file
  *
@@ -10,6 +10,9 @@
  * May 8, 2020 (IC/JD V1.1)
  * - Add called to addApplicationFont, since newer versions of Qt (apparently)
  *   need this to use the included fonts.
+ * May 12, 2020 (IC V1.2)
+ *  (a) Added another font (arimo.ttf) to be embedded, so that Graphic
+ *      can look as similar as possible on different systems. 
  */
 
 #include "mainwindow.h"
@@ -26,9 +29,9 @@ main(int argc, char * argv[])
 
     QFontDatabase::addApplicationFont(":/fonts/cmmi10.ttf");
     QFontDatabase::addApplicationFont(":/fonts/cmr10.ttf");
+    QFontDatabase::addApplicationFont(":/fonts/arimo.ttf");
 
     MainWindow w;
     w.show();
-
     return a.exec();
 }
